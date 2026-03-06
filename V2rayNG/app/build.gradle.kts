8594318f8593def60be9e0bee374f881cc5be4f2
KindlySchultz <KindlySchultz@proton.me>
2026-03-06 14:47:31 +0800

    Update build.gradle.kts

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.v2ray.ang"
+        applicationId = "cn.ccspeed.hw"
         minSdk = 24
         targetSdk = 36
         versionCode = 713
